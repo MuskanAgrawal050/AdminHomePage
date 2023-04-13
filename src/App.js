fa569ee5c172464ba254f9a8331c2d6444ae5193
Agrawal <850066565@genpact.com>
2023-04-13 16:39:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+import logo from './finzlogo.png';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="main">
+<div class="header">
+<header className="App-header">
+<img src = {logo} className="App-logo" alt="finzlogo" />
+
+</header>
+</div>
+<div className="body">
+<div className='body__up'></div>
+<h1>Hi Admin</h1>
+
+</div>
+<div className="body__down">
+<button>Account Request</button>
+<button>Existing User</button>
+<button>Support Request</button>
+</div>
+</div>
+
+);
+
 }
 
 export default App;
